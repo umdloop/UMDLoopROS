82434cb39c486b78929fb795bc35be977a5d28c9
Mohammad Durrani <mdurrani808@gmail.com>
2024-02-08 21:57:20 -0500

    first pass at hardware interface

---
+++ src/diff_drive/hardware/include/hardware_int/drive.hpp
@@ -0,0 +1,60 @@
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "hardware_interface/handle.hpp"
+#include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "rclcpp/clock.hpp"
+#include "rclcpp/duration.hpp"
+#include "rclcpp/macros.hpp"
+#include "rclcpp/time.hpp"
+#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
+#include "rclcpp_lifecycle/state.hpp"
+
+
+namespace hardware_int
+{
+class DiffBotSystemHardware : public hardware_interface::SystemInterface
+{
+public:
+  //RCLCPP_SHARED_PTR_DEFINITIONS(DiffBotSystemHardware);
+
+
+  hardware_interface::CallbackReturn on_init(
+    const hardware_interface::HardwareInfo & info) override;
+
+
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+  hardware_interface::CallbackReturn on_activate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  hardware_interface::CallbackReturn on_deactivate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  hardware_interface::return_type read(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+  hardware_interface::return_type write(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+private:
+  // Parameters for the DiffBot simulation
+  double hw_start_sec_;
+  double hw_stop_sec_;
+
+  // Store the command for the simulated robot
+  std::vector<double> hw_commands_;
+  std::vector<double> hw_positions_;
+  std::vector<double> hw_velocities_;
+  //The output shaft angle in degrees with a resolution of 1 deg and a maximum range of 32767
+  //goes from degrees, to motor rotations, to output rotations, to meters (covered by rotation of the wheel)
+  float convertShaftAngleToRotations(float shaftAngle);
+};
+
+}  // namespace ros2_control_demo_example_2
+
---
+++ src/diff_drive/hardware/src/drive.cpp
@@ -0,0 +1,176 @@
+// Copyright 2021 ros2_control Development Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "hardware_int/drive.hpp"
+
+#include <chrono>
+#include <cmath>
+#include <cstddef>
+#include <limits>
+#include <memory>
+#include <vector>
+#include "pluginlib/class_list_macros.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include <myactuator_rmd/driver.hpp>
+using namespace hardware_interface;
+using namespace std;
+using namespace myactuator_rmd;
+
+namespace hardware_int {
+
+Driver left_back{"can0", 99};
+Driver left_middle{"can0", 99};
+Driver left_front{"can0", 99};
+
+Driver right_back{"can0", 99};
+Driver right_middle{"can0", 99};
+Driver right_front{"can0", 99};
+
+vector<Driver*> motors = {&left_back, &left_middle, &left_front, &right_back, &right_middle, &right_front};
+
+CallbackReturn DiffBotSystemHardware::on_init(const HardwareInfo & info) {
+  if (SystemInterface::on_init(info) != CallbackReturn::SUCCESS) {
+    return CallbackReturn::ERROR;
+  }
+  
+  hw_positions_.resize(info_.joints.size(), numeric_limits<double>::quiet_NaN());
+  hw_velocities_.resize(info_.joints.size(), numeric_limits<double>::quiet_NaN());
+  hw_commands_.resize(info_.joints.size(), numeric_limits<double>::quiet_NaN());
+
+  for (const ComponentInfo & joint : info_.joints) {
+    // DiffBotSystem has exactly two states and one command interface on each joint
+    if (joint.command_interfaces.size() != 1) {
+      RCLCPP_FATAL(
+        rclcpp::get_logger("DiffBotSystemHardware"),
+        "Joint '%s' has %zu command interfaces found. 1 expected.", joint.name.c_str(),
+        joint.command_interfaces.size());
+      return CallbackReturn::ERROR;
+    }
+
+    if (joint.command_interfaces[0].name != HW_IF_VELOCITY) {
+      RCLCPP_FATAL(
+        rclcpp::get_logger("DiffBotSystemHardware"),
+        "Joint '%s' have %s command interfaces found. '%s' expected.", joint.name.c_str(),
+        joint.command_interfaces[0].name.c_str(), HW_IF_VELOCITY);
+      return CallbackReturn::ERROR;
+    }
+
+    if (joint.state_interfaces.size() != 2) {
+      RCLCPP_FATAL(
+        rclcpp::get_logger("DiffBotSystemHardware"),
+        "Joint '%s' has %zu state interface. 2 expected.", joint.name.c_str(),
+        joint.state_interfaces.size());
+      return CallbackReturn::ERROR;
+    }
+
+    if (joint.state_interfaces[0].name != HW_IF_POSITION) {
+      RCLCPP_FATAL(
+        rclcpp::get_logger("DiffBotSystemHardware"),
+        "Joint '%s' have '%s' as first state interface. '%s' expected.", joint.name.c_str(),
+        joint.state_interfaces[0].name.c_str(), HW_IF_POSITION);
+      return CallbackReturn::ERROR;
+    }
+
+    if (joint.state_interfaces[1].name != HW_IF_VELOCITY) {
+      RCLCPP_FATAL(
+        rclcpp::get_logger("DiffBotSystemHardware"),
+        "Joint '%s' have '%s' as second state interface. '%s' expected.", joint.name.c_str(),
+        joint.state_interfaces[1].name.c_str(), HW_IF_VELOCITY);
+      return CallbackReturn::ERROR;
+    }
+  }
+
+  return CallbackReturn::SUCCESS;
+}
+
+vector<StateInterface> DiffBotSystemHardware::export_state_interfaces() {
+  vector<StateInterface> state_interfaces;
+  for (auto i = 0u; i < info_.joints.size(); i++)
+  {
+    state_interfaces.emplace_back(StateInterface(info_.joints[i].name, HW_IF_POSITION, &hw_positions_[i]));
+    state_interfaces.emplace_back(StateInterface(info_.joints[i].name, HW_IF_VELOCITY, &hw_velocities_[i]));
+  }
+
+  return state_interfaces;
+}
+
+vector<CommandInterface> DiffBotSystemHardware::export_command_interfaces() {
+  vector<CommandInterface> command_interfaces;
+  for (auto i = 0u; i < info_.joints.size(); i++)
+  {
+    command_interfaces.emplace_back(CommandInterface(
+      info_.joints[i].name, HW_IF_VELOCITY, &hw_commands_[i]));
+  }
+
+  return command_interfaces;
+}
+
+CallbackReturn DiffBotSystemHardware::on_activate(const rclcpp_lifecycle::State & /*previous_state*/) {
+  // set some default values
+  for (auto i = 0u; i < hw_positions_.size(); i++)
+  {
+    if (isnan(hw_positions_[i]))
+    {
+      hw_positions_[i] = 0;
+      hw_velocities_[i] = 0;
+      hw_commands_[i] = 0;
+    }
+  }
+
+  RCLCPP_INFO(rclcpp::get_logger("DiffBotSystemHardware"), "Successfully activated!");
+
+  return CallbackReturn::SUCCESS;
+}
+
+CallbackReturn DiffBotSystemHardware::on_deactivate(const rclcpp_lifecycle::State & /*previous_state*/) {
+  for (auto i = 0u; i < hw_positions_.size(); i++) {
+    if (isnan(hw_positions_[i]))
+    {
+      hw_positions_[i] = 0;
+      hw_velocities_[i] = 0;
+      hw_commands_[i] = 0;
+    }
+  }
+  RCLCPP_INFO(rclcpp::get_logger("DiffBotSystemHardware"), "Successfully deactivated!");
+  return CallbackReturn::SUCCESS;
+}
+
+return_type DiffBotSystemHardware::read(const rclcpp::Time & /*time*/, const rclcpp::Duration & period) {
+  for(size_t i = 0; i < hw_positions_.size(); i++) {
+    hw_positions_[i] = convertShaftAngleToRotations(motors[i]->getMotorStatus2().shaft_angle);
+
+    RCLCPP_INFO(rclcpp::get_logger("DiffBotSystemHardware"), "Got position state of %.5f for %s!", hw_positions_[i], info_.joints[i].name.c_str());
+  }
+  return return_type::OK;
+}
+
+return_type hardware_int::DiffBotSystemHardware::write(
+  const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/) {
+  RCLCPP_INFO(rclcpp::get_logger("DiffBotSystemHardware"), "Writing...");
+  for(auto i = 0u; i<hw_commands_.size(); i++) {
+      RCLCPP_INFO(
+      rclcpp::get_logger("DiffBotSystemHardware"), "Got command %.5f for '%s'!", hw_commands_[i],
+      info_.joints[i].name.c_str());
+      motors[i]->sendVelocitySetpoint(convertShaftAngleToRotations(hw_commands_[i]));
+  }
+  return return_type::OK;
+}
+
+}
+float convertShaftAngleToRotations(float shaftAngle)  {
+    return shaftAngle * (1.0/360.0) * (36.0/1.0) * ((2.0*3.14159*.2667)/1.0);
+  }
+PLUGINLIB_EXPORT_CLASS(
+  hardware_int::DiffBotSystemHardware, SystemInterface)
