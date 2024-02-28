69d873dac582034b76f223239cb136598b45fe35
Mohammad Durrani <mdurrani808@gmail.com>
2024-02-27 20:26:26 -0500

    lets get it

--- src/diff_drive/launch/diffbot.launch.py
+++ src/diff_drive/launch/diffbot.launch.py
@@ -35,7 +35,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "use_mock_hardware",
-            default_value="false",
+            default_value="true",
             description="Start robot with mock hardware mirroring command to its states.",
         )
     )
